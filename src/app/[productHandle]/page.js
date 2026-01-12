a68079dc1edcd4d2dfbf85a82c45e73ba8b9889c
Tom Valentino <tommyvalentino420@gmail.com>
2026-01-12 17:15:43 +0700

    new

--- src/app/[productHandle]/page.js
+++ src/app/[productHandle]/page.js
@@ -1,19 +1,28 @@
-
-export const dynamic = "force-static"      // ensure SSG
-export const revalidate = false            // no runtime fetches
+export const dynamic = "force-static"
+export const revalidate = false
 
 import { getAllProducts, getProduct } from "@/lib/redis"
 const storeId = process.env.NEXT_PUBLIC_STORE_ID
 
+// --- STATIC PARAMS (SSG) ---
+export async function generateStaticParams() {
+  const res = await getAllProducts(storeId)
+  if (!res.ok) return []
+
+  const params = res.data.map((p) => {
+    console.log("[BUILD] Prebuilding:", p.handle)
+    return { productHandle: p.handle }
+  })
+
+  return params
+}
+
 // --- METADATA (SSG) ---
 export async function generateMetadata({ params }) {
   const { productHandle } = await params
-
   const product = await getProduct(storeId, productHandle)
 
-  if (!product.ok) {
-    return { title: "Product not found" }
-  }
+  if (!product.ok) return { title: "Product not found" }
 
   return {
     title: product.data.title,
@@ -21,29 +30,12 @@ export async function generateMetadata({ params }) {
   }
 }
 
-// --- STATIC PARAMS (SSG) ---
-export async function generateStaticParams() {
-  const res = await getAllProducts(storeId)
-  if (!res.ok) return []
-
-  return res.data.map((p) => ({
-    productHandle: p.handle
-  }))
-}
-
 // --- PAGE (SSG) ---
 export default async function Page({ params }) {
   const { productHandle } = await params
-
   const product = await getProduct(storeId, productHandle)
 
-  if (!product.ok) {
-    return <h1>No product found</h1>
-  }
+  if (!product.ok) return <h1>No product found</h1>
 
-  return (
-    <>
-      <h1>{product.data.title}</h1>
-    </>
-  )
+  return <h1>{product.data.title}</h1>
 }
