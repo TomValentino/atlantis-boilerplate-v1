45739c6f8097cda465b3509a14566e7f28b3f4df
Tom Valentino <tommyvalentino420@gmail.com>
2026-01-19 14:58:46 +0700

    changes

--- src/app/collections/[collectionHandle]/client-page.jsx
+++ src/app/collections/[collectionHandle]/client-page.jsx
@@ -7,32 +7,121 @@ export default function ClientPage({ initialProducts }) {
   const [products, setProducts] = useState(initialProducts);
 
   const collection = collectionPageState.use('collection')
+  const isLoading = collectionPageState.use('isLoading')
 
+  if (isLoading) return <>Loading state......</>
   if (!collection) return <>NO COLLECTION !</>
 
-  // After hydration, instantly replace with history
-//   useLayoutEffect(() => {
-//     try {
-//       const saved = localStorage.getItem("history_products");
-//       if (saved) {
-//         setProducts(JSON.parse(saved));
-//       }
-//     } catch {}
-//   }, []);
+
 
   const addMoreProducts = (newProducts) => {
     const updated = [...products, ...newProducts];
-    setProducts(updated);
-    localStorage.setItem("history_products", JSON.stringify(updated));
+    const newCollection = {
+        ...collection,
+        products: updated,
+    }
+    console.log('UPDATE', updated)
+    localStorage.setItem(`collection-state-${collection.handle}`, JSON.stringify(newCollection));
   };
 
   return (
     <div>
         <pre>{JSON.stringify(collection.products, null, 2)}</pre>
       {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
-      <button onClick={() => addMoreProducts([{ id: Math.random(), title: "New" }])}>
+      <button onClick={() => addMoreProducts([product])}>
         Add More
       </button>
     </div>
   );
 }
+
+
+const product = {
+    "id": "gid://shopify/Product/3443344334",
+    "handle": "sugar-baby-doll-bustier-intimate-set-stocking-pink",
+    "title": "Sugar Baby Doll Bustier Intimate Set + Stocking - Pink",
+    "featured_image": {
+      "url": "https://cdn.shopify.com/s/files/1/0500/1055/4539/files/IMG_88042.jpg?v=1738855257",
+      "alt": ""
+    },
+    "images": [],
+    "variants": [
+      {
+        "id": "gid://shopify/ProductVariant/47031706583272",
+        "title": "XS",
+        "price": "339000.0",
+        "compare_at_price": "499000.0",
+        "selected_options": [
+          {
+            "name": "Size",
+            "value": "XS"
+          }
+        ]
+      },
+      {
+        "id": "gid://shopify/ProductVariant/47031706616040",
+        "title": "S",
+        "price": "339000.0",
+        "compare_at_price": "499000.0",
+        "selected_options": [
+          {
+            "name": "Size",
+            "value": "S"
+          }
+        ]
+      },
+      {
+        "id": "gid://shopify/ProductVariant/47031706648808",
+        "title": "M",
+        "price": "339000.0",
+        "compare_at_price": "499000.0",
+        "selected_options": [
+          {
+            "name": "Size",
+            "value": "M"
+          }
+        ]
+      },
+      {
+        "id": "gid://shopify/ProductVariant/47031706681576",
+        "title": "L",
+        "price": "339000.0",
+        "compare_at_price": "499000.0",
+        "selected_options": [
+          {
+            "name": "Size",
+            "value": "L"
+          }
+        ]
+      },
+      {
+        "id": "gid://shopify/ProductVariant/47031706714344",
+        "title": "XL",
+        "price": "339000.0",
+        "compare_at_price": "499000.0",
+        "selected_options": [
+          {
+            "name": "Size",
+            "value": "XL"
+          }
+        ]
+      },
+      {
+        "id": "gid://shopify/ProductVariant/47031706747112",
+        "title": "XXL",
+        "price": "339000.0",
+        "compare_at_price": "499000.0",
+        "selected_options": [
+          {
+            "name": "Size",
+            "value": "XXL"
+          }
+        ]
+      }
+    ],
+    "price": 339000,
+    "max_price": 339000,
+    "metafield": {
+      "value": "Pink"
+    }
+  }
--- src/store/collection/collection-state.jsx
+++ src/store/collection/collection-state.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { createCustomState } from "@/lib/state";
+import { useRouter } from "next/navigation";
 import { useEffect, useLayoutEffect, useRef } from "react";
 
 
@@ -35,10 +36,14 @@ export const collectionState = createCustomState(
  export const collectionPageState = createCustomState(
   {
     collection: {},  // main collection
+    isLoading: true,
   },
   {
     setCollection: ({ set, setKey }, collection) => {
       set("collection", collection);
+    },
+    setIsLoading: ({ set, setKey }, value) => {
+      set("isLoading", value);
     },
   }
 );
@@ -48,26 +53,33 @@ export const collectionState = createCustomState(
 
 export const SetupCollectionPageState = ({ collection, children }) => {
 
+    // const didInit = useRef(false);
+    // if (!didInit.current) {
+      // didInit.current = true;
 
-    const didInit = useRef(false);
-    if (!didInit.current) {
-      didInit.current = true;
+      useEffect(() => {
 
-      let initial = collection;
-  
-      if (typeof window !== "undefined") {
-        const saved = localStorage.getItem(`collection-state-${collection.id}`);
-  
-        if (saved) {
-          try {
-            initial = JSON.parse(saved);
-          } catch (_) {}
+        let initial = collection;
+    
+        if (typeof window !== "undefined") {
+          const saved = localStorage.getItem(`collection-state-${collection.handle}`);
+    
+          if (saved) {
+            try {
+              initial = JSON.parse(saved);
+              console.log('initail', initial)
+            } catch (_) {}
+          }
         }
-      }
+          // Hydrate instantly before paint
+          collectionPageState.setCollection(initial);
+          collectionPageState.setIsLoading(false);
+          
+        
+      }, [])
+
   
-      // Hydrate instantly before paint
-      collectionPageState.setCollection(initial);
-    }
+    // }
 
 
 
